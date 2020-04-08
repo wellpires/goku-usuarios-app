170ed0027b1622bee7c3728985f20f06e8359f53
wellpires <wellington.goncalves@live.com>
2020-04-08 13:59:39 -0300

    Commit inicial

---
+++ src/main/java/com/goku/usuarios/GokuUsuariosAppApplication.java
@@ -0,0 +1,13 @@
+package com.goku.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokuUsuariosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuUsuariosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goku/usuarios/GokuUsuariosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goku.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GokuUsuariosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
